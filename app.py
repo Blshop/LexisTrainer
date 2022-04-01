aba70490c5cf3c845e8da42ac62ef8e31187937f
Nikita_Erechtchenko <envworking@gmail.com>
2022-04-01 13:55:11 +0300

    minor changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -17,9 +17,15 @@ class Russian(db.Model):
     word = db.Column('word', db.String(100))
     part = db.Column('part', db.String(20))
 
+    def __init__(self, word, part):
+        self.word = word
+        self.part = part
+
 class Translation(db.Model):
     english_id = db.Column('english_id', db.Integer, db.ForeignKey('english.id'), primary_key=True)
     russian_id = db.Column('russian_id', db.Integer, db.ForeignKey('russian.id'), primary_key=True)
+
+
 
 
 @app.route("/")
@@ -27,8 +33,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/addword")
+@app.route("/addword", methods=['GET', 'POST'])
 def add_word():
+    if request.method == 'POST':
+        translations = request.form['translation'].split('/')
+        for item in translations:
+            word = Russian(item, request.form['part'])  
+            db.session.add(word)
+        db.session.commit()
+        # flash('Record was successfully added')
+        return redirect(url_for('index'))
     return render_template("AddWords.html")
 
 
