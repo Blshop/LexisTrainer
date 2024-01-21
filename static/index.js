bb5f52e5b24838e937dd1964cf8dc9e4c4e58f28
Blshop <envworking@gmail.com>
2024-01-21 22:18:18 +0300

    add comments index.js

--- static/index.js
+++ static/index.js
@@ -1,24 +1,23 @@
+// load selected languages from DB
 let active_languages = JSON.parse(
   document
     .querySelector('meta[name="active_languages"]')
     .getAttribute('data-active_languages')
 )
 
-var primary_element
-var secondary_element
+// variables to store primary and secondary languages
+var primary_element = document.getElementById(
+  'primary-' + active_languages['primary_language']
+)
+var secondary_element = document.getElementById(
+  'secondary-' + active_languages['secondary_language']
+)
 
-if (active_languages == null) {
-  primary_element = null
-  secondary_element = null
-} else {
-  primary_element = document.getElementById(
-    'primary-' + active_languages['primary_language']
-  )
-  secondary_element = document.getElementById(
-    'secondary-' + active_languages['secondary_language']
-  )
+// apply previously set languages
+if (active_languages != null) {
   primary(primary_element, secondary_element)
 }
+
 
 function lang_select() {
   if (primary_element != null && secondary_element != null) {
@@ -36,21 +35,21 @@ function lang_select() {
 }
 
 function primary(new_element) {
-  if (new_element.classList.contains('active')) {
-    new_element.classList.remove('active')
+  if (new_element.classList.contains('selected')) {
+    new_element.classList.remove('selected')
     var secondaries = document.getElementById('secondary-language').children
     for (i = 0; i < secondaries.length; i++) {
       secondaries[i].classList.add('inactive')
-      secondaries[i].classList.remove('active')
+      secondaries[i].classList.remove('selected')
     }
     primary_element = null
     secondary_element = null
     console.log(primary_element)
   } else {
     if (primary_element != null) {
-      primary_element.classList.remove('active')
+      primary_element.classList.remove('selected')
     }
-    new_element.classList.add('active')
+    new_element.classList.add('selected')
     primary_element = new_element
     secondary(secondary_element)
   }
@@ -60,14 +59,14 @@ function secondary(new_element = null) {
   var secondaries = document.getElementById('secondary-language').children
   for (i = 0; i < secondaries.length; i++) {
     secondaries[i].classList.remove('inactive')
-    secondaries[i].classList.remove('active')
+    secondaries[i].classList.remove('selected')
   }
   document
     .getElementById('secondary-' + primary_element.innerHTML)
     .classList.add('inactive')
   if (new_element != null) {
     secondary_element = new_element
-    secondary_element.classList.add('active')
+    secondary_element.classList.add('selected')
   }
   lang_select()
 }
@@ -76,6 +75,6 @@ function set_default() {
   var secondaries = document.getElementById('secondary-language').children
   for (i = 0; i < secondaries.length; i++) {
     secondaries[i].classList.add('inactive')
-    secondaries[i].classList.remove('active')
+    secondaries[i].classList.remove('selected')
   }
 }
